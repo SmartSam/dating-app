a0f5ae37b762740d750a0244a10a19d3442e4a7d
Sam Smart <ssmart108@gmail.com>
2020-07-17 19:12:54 -0600

    UI work

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -6,6 +6,9 @@ import {
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 import { AppComponent } from './app.component';
 import { fromEventPattern } from 'rxjs';
@@ -16,11 +19,20 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { RouterModule } from '@angular/router';
 import {appRoutes} from './routes';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MembersDetailComponent } from './members/members-detail/members-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-lists.resolver';
+
+
+export function tokenGetter() {
+   return localStorage.getItem('token');
+ }
 
 
 @NgModule({
@@ -31,7 +43,9 @@ import {appRoutes} from './routes';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent,
+      MembersDetailComponent
    ],
    imports: [
       BrowserModule,
@@ -39,14 +53,25 @@ import {appRoutes} from './routes';
       BrowserAnimationsModule,
       FormsModule,
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
+      RouterModule.forRoot(appRoutes),
+      NgxGalleryModule,
+      JwtModule.forRoot({
+         config: {
+           tokenGetter,
+           allowedDomains: ['localhost:5000'],
+           disallowedRoutes: ['localhost:5000/api/auth']
+         }
+      }),
+      TabsModule.forRoot()
    ],
    providers: [
       AuthService,
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      MemberDetailResolver,
+      MemberListResolver
    ],
    bootstrap: [
       AppComponent
    ]
 })
-export class AppModule {}
+export class AppModule { }
