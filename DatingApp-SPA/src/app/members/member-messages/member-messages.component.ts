efb6a27a9ed10b91b4724157164e42162e2e05dc
Sam Smart <ssmart108@gmail.com>
2020-08-20 17:40:09 -0600

    Messaging

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -3,6 +3,7 @@ import { Message } from 'src/app/_models/message';
 import { UserService } from 'src/app/_services/user.service';
 import { AuthService } from 'src/app/_services/auth.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-messages',
@@ -24,17 +25,32 @@ export class MemberMessagesComponent implements OnInit {
     this.loadMessages();
   }
   loadMessages() {
+    const currentUserId = +this.authService.decodedToken.nameid;
     this.userService
       .getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+      .pipe(
+        tap(messages => {
+          // tslint:disable-next-line: prefer-for-of
+          for (let i = 0; i < messages.length; i++) {
+            if (
+              messages[i].isRead === false &&
+              messages[i].recipientId === currentUserId
+            ) {
+              this.userService.markAsRead(currentUserId, messages[i].id);
+            }
+          }
+        })
+      )
       .subscribe(
-        (messages) => {
+        messages => {
           this.messages = messages;
         },
-        (error) => {
+        error => {
           this.alertify.error(error);
         }
       );
   }
+
   sendMessage() {
     this.newMessage.recipientId = this.recipientId;
     this.userService
